85832b987ed9229a80f4671b6d99c2273ad70bab
raulpsingh <raulpsingh2@gmail.com>
2021-04-21 02:01:22 +0300

    Calc

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Simple calculator';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: AddTwoNumbers(),
+      ),
+    );
+  }
+}
+class AddTwoNumbers extends StatefulWidget {
+  @override
+  _AddTwoNumbersState createState() => _AddTwoNumbersState();
+}
+
+class _AddTwoNumbersState extends State<AddTwoNumbers> {
+  TextEditingController num1controller = new TextEditingController();
+  TextEditingController num2controller = new TextEditingController();
+  String resulttext = "0";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text("Number 1 : "),
+              new Flexible(
+                child: new TextField(
+                  keyboardType: TextInputType.number,
+                  controller: num1controller,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text("Number 2 : "),
+              new Flexible(
+                child: new TextField(
+                  keyboardType: TextInputType.number,
+                  controller: num2controller,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("+"),
+                onPressed : () {
+                  setState(() {
+                    int result = int.parse(num1controller.text) + int.parse(num2controller.text);
+                    resulttext = result.toString();
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text("-"),
+                onPressed : () {
+                  setState(() {
+                    int result = int.parse(num1controller.text) - int.parse(num2controller.text);
+                    resulttext = result.toString();
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text("x"),
+                onPressed : () {
+                  setState(() {
+                    int result = int.parse(num1controller.text) * int.parse(num2controller.text);
+                    resulttext = result.toString();
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text("/"),
+                onPressed : () {
+                  setState(() {
+                    double result = double.parse(num1controller.text)/double.parse(num2controller.text);
+                    resulttext = result.toStringAsPrecision(3);
+                  });
+                },
+              ),
+            ],
+          ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+
+              RaisedButton(
+                child: Text("^"),
+                onPressed : () {
+                  setState(() {
+                    double result =pow(double.parse(num1controller.text), double.parse(num2controller.text));
+                    resulttext = result.toStringAsPrecision(3);
+                  });
+                },
+              ),
+           ],
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Result:",
+                style: TextStyle(
+                  fontSize: 30,
+                ),),
+              new Text(resulttext,
+                style: TextStyle(
+                  fontSize: 30,
+                ),),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
